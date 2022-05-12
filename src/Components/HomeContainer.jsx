56aa1c25627655d14f2006a357fe2884603584dd
jokatty <jyotikattani@gmail.com>
2022-05-12 17:24:47 +0200

    add home page components

---
+++ src/Components/HomeContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
+export default function HomeContainer(){
+  return (
+    <Container maxWidth="sm">
+    <Grid container>
+      <Grid item xs={6}>
+    <p>Hi</p>
+      </Grid>
+      <Grid item xs = {6}>
+<p>hey</p>
+      </Grid>
+    </Grid>
+    </Container>
+  )
+}
