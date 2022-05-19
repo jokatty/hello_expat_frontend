93b2141a4ffeeb91df2a4720e11d2dfe2e993261
jokatty <jyotikattani@gmail.com>
2022-05-19 16:16:50 +0200

    add routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';
 
 function App() {
   return (
-    <HomePage />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
