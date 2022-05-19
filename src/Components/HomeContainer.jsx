58dfa21313d7fc4d85402e8b75d79e47c998dfd3
jokatty <jyotikattani@gmail.com>
2022-05-19 15:32:44 +0200

    add buttons forposts and experiences

--- src/Components/HomeContainer.jsx
+++ src/Components/HomeContainer.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import { Button } from '@mui/material';
 
 export default function HomeContainer() {
+  // style object
+  const container = {
+    display: 'flex',
+    alignItem: 'center',
+    justifyContent: 'center',
+    marginTop: '15vh',
+  };
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="sm" style={container}>
       <Grid container>
         <Grid item xs={6}>
-          <p>Hi</p>
+          <Button variant="contained">Post a question</Button>
         </Grid>
         <Grid item xs={6}>
-          <p>hey</p>
+          <Button variant="contained">Share an experience</Button>
         </Grid>
       </Grid>
     </Container>
