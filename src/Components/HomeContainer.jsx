990eac6bab68f2fa632694bcae305ddaaf41f4c5
jokatty <jyotikattani@gmail.com>
2022-05-19 17:07:54 +0200

    add call back for the post question btn

--- src/Components/HomeContainer.jsx
+++ src/Components/HomeContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomeContainer() {
   // style object
@@ -11,11 +12,26 @@ export default function HomeContainer() {
     justifyContent: 'center',
     marginTop: '15vh',
   };
+
+  /**
+   * on click direct to /post-question route
+   */
+  const navigate = useNavigate();
+
+  const questionRoute = () => {
+    navigate('/question');
+  };
   return (
     <Container maxWidth="sm" style={container}>
       <Grid container>
         <Grid item xs={6}>
-          <Button variant="contained">Post a question</Button>
+          <Button
+            variant="contained"
+            onClick={() => questionRoute()}
+          >
+            Post a question
+
+          </Button>
         </Grid>
         <Grid item xs={6}>
           <Button variant="contained">Share an experience</Button>
