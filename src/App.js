8fb0ceaef50444a5fed2fa1f21da64f9d64aebc7
jokatty <jyotikattani@gmail.com>
2022-05-19 17:08:57 +0200

    add question route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';
+import Form from './Components/Form';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/question" element={<Form />} />
       </Routes>
     </Router>
   );
